bb38d30f8fdda5898bb28f391df43f66f0282e35
Yulia <strekozinok@gmail.com>
2023-10-02 18:16:20 +0400

    Version 1 and Git ignore

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from pydrive.drive import GoogleDrive
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
+folder_id = '1FNuu7w_3hDVKDbiTBXJnbrOdGhYLDdQV'
+upload_file = '/Users/yuliaustinova/Documents/Документы/Резюме/Резюме Юлия Устинова Аналитик данных.docx'
+
+gfile = drive.CreateFile({
+    'title': ['Резюме Юлия Устинова Аналитик данных.docx'],
+    'parents': [{'id': folder_id}]
+    })
+gfile.SetContentFile(upload_file)
+gfile.Upload()
